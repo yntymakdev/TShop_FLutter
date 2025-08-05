53fde7d3ff3471094e6b4ae4c15c62fb8fa3b519
yntymakdev <kubanychevyntymak21@gmail.com>
2025-08-05 15:27:14 +0600

    s

--- lib/utils/theme/theme.dart
+++ lib/utils/theme/theme.dart
@@ -2,6 +2,13 @@ import 'package:flutter/material.dart';
 
 class TAppTheme {
   TAppTheme._();
-  static ThemeData lightTheme = ThemeData();
-  static
+  static ThemeData lightTheme = ThemeData(
+    useMaterial3: true,
+    fontFamily: 'Poppins',
+    brightness: Brightness.light,
+    primaryColor: Colors.blue,
+    scaffoldBackgroundColor: Colors.white,
+    textTheme: TextTheme(),
+  );
+  static ThemeData darkTheme = ThemeData();
 }
